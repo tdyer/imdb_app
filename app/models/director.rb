9659bf89bd85cfdb4f7edb06abf9c1691e0f1429
Tom Dyer <tdyer1@gmail.com>
2013-10-16 23:21:23 -0400

    added validations for models

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,7 @@
 class Director < ActiveRecord::Base
 
   has_many :movies
+
+  validates :name, :dob, presence: true
   
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,10 @@ class Movie < ActiveRecord::Base
   RATINGS = %w{G PG PG-13 R X}
 
   belongs_to :director
-  
+
+  validates :name, :release_date, :rating, presence: true
+  validates :name, length: { in: 5..20 }
+  validates :length, numericality: { less_than: 300 }
+  validates :rating, inclusion: { in: RATINGS }
+  # validates :release_date
 end
