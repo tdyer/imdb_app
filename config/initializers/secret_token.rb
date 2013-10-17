67d550b234741b5b8a461b0a7909ef45904985fc
Tom Dyer <tdyer1@gmail.com>
2013-10-16 21:37:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImdbApp::Application.config.secret_key_base = 'd3f0590435fbda206ad41229f1dae446eee5fff1133f65bdd9ae575865b642a642136e684e21ba3c6a23f31ecfbf807d594cfe99b759d6a5099d9566934984ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImdbApp::Application.config.session_store :cookie_store, key: '_imdb_app_session'
